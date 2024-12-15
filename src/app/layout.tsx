3bc83200f252be3249d9d9d2c5b33cf53b0a2842
Prantik Kumar Seal <83081442+prantikseal@users.noreply.github.com>
2024-12-15 21:05:21 +0530

    Add Google Tag Manager and Umami tracking script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Plus_Jakarta_Sans, Manrope } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -29,10 +30,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="f67b5b4d-ae5b-41ba-aa86-3af1ee94d39e"
+        ></script>
+
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W3WW722C');
+          `}
+        </Script>
+      </head>
       <body
         className={`${plusJakarta.variable} ${manrope.variable} ${inter.variable} font-sans antialiased`}
       >
         {children}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3WW722C"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
